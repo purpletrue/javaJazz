fdbc88a466a6e30b14b477d8449e3c2507c64e88
Eugene <129924532+purpletrue@users.noreply.github.com>
2023-06-08 17:59:23 +0900

    Main.java

---
+++ extra/Main.java
@@ -0,0 +1,24 @@
+package extra;
+
+public class Main {
+    public static void main(String[] args) {
+        AmigosCode amigos = new AmigosCode("id1", "yujin", 1996);
+
+        int birthYear = amigos.getBirthYear();
+
+        System.out.println(amigos);
+        System.out.println(birthYear);
+        System.out.println(amigos.getUsername());
+        System.out.println(amigos.getId());
+
+
+    }
+
+    public void setUserInfo(String id, String username, int birthYear) {
+
+        String x1;
+        String x2;
+        int xx3;
+
+    }
+}
